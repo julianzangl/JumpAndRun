8cbd90bd24da5f4cfe57ec2245554f4d6bcb3c77
julianzangl <julian.zangl@outlook.com>
2026-04-05 14:43:11 +0200

    feat(character): :sparkles: add new character and lever scripts with interaction functionality fix(input): 🐛 update input actions for interact and jump chore(prefabs): 🔧 update mage and lever prefabs with new colliders

---
+++ Assets/Scripts/JnR/Lever.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Lever : MonoBehaviour
+{
+    private bool on = false;
+    private bool interpolating = false;
+    private float currentInterpolationTime = 0.0f;
+    private bool playerInRange = false;
+    private InputAction interactAction;
+    [SerializeField]
+    private float switchTime;
+    [SerializeField]
+    private Transform onPosition;
+    [SerializeField]
+    private Transform offPosition;
+    [SerializeField]
+    private GameObject leverHandle;
+    [SerializeField]
+    private CapsuleCollider playerCollider;
+
+    void Start()
+    {
+        interactAction = InputSystem.actions.FindAction("Interact");
+        interactAction.performed += OnInteract;
+    }
+
+    void OnDestroy()
+    {
+        interactAction.performed -= OnInteract;
+    }
+
+    void OnInteract(InputAction.CallbackContext context)
+    {
+        if (!interpolating && playerInRange)
+        {
+            ToggleLever();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == playerCollider.gameObject)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == playerCollider.gameObject)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == playerCollider.gameObject)
+        {
+            playerInRange = false;
+        }
+    }
+
+    IEnumerator InterpolateLeverCoroutine()
+    {
+        interpolating = true;
+        Vector3 startPosition, targetPosition;
+        Quaternion startRotation, targetRotation;
+        if (on)
+        {
+            startPosition = offPosition.position;
+            startRotation = offPosition.rotation;
+            targetPosition = onPosition.position;
+            targetRotation = onPosition.rotation;
+        }
+        else
+        {
+            startPosition = onPosition.position;
+            startRotation = onPosition.rotation;
+            targetPosition = offPosition.position;
+            targetRotation = offPosition.rotation;
+        }
+        currentInterpolationTime = 0.0f;
+        while (currentInterpolationTime < switchTime)
+        {
+            float percentage = currentInterpolationTime / switchTime;
+            var currentPosition = Vector3.Lerp(startPosition, targetPosition, percentage);
+            var currentRotation = Quaternion.Slerp(startRotation, targetRotation, percentage);
+            leverHandle.transform.SetPositionAndRotation(currentPosition, currentRotation);
+            yield return null;
+            currentInterpolationTime += Time.deltaTime;
+        }
+        leverHandle.transform.SetPositionAndRotation(targetPosition, targetRotation);
+        interpolating = false;
+    }
+    void ToggleLever()
+    {
+        on = !on;
+        StartCoroutine(InterpolateLeverCoroutine());
+    }
+
+}
